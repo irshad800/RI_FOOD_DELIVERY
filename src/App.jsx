489f4d14d117057fcd66b4d819d803e17b22bccd
irshad <irshadvp800@gmail.com>
2025-12-10 11:34:59 +0400

    add router app.jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import NavaBar from './components/NavBar/NavaBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/home'
+import Cart from './pages/cart/cart'
+import PlaceOrder from './pages/placeOrder/placeOrder'
+
 
 const App = () => {
   return (
     <div className='app'>
 <NavaBar/>
+<Routes>
+  <Route path='/' element={<Home/>}/>
+  <Route path='/cart' element={<Cart/>}/>
+  <Route path='/place-order' element={<PlaceOrder/>}/>
+</Routes>
       
     </div>
   )
