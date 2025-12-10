a8c1f00e7013649838ef3af4c750a33eb6c3ef73
irshad <irshadvp800@gmail.com>
2025-12-10 14:48:40 +0400

    adder header part in home section completed

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>RI FOOD DELIVERY</h2>
+            <p>Get your favorite food delivered to your doorstep, fresh, fast, and hassle-free—anytime you crave it.  </p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
