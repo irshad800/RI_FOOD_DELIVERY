740c914e29f147d7294b2722c142c13c2c20f164
irshad <irshadvp800@gmail.com>
2025-12-10 11:10:13 +0400

    navigation bar component completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavaBar from './components/NavBar/NavaBar'
+
+const App = () => {
+  return (
+    <div className='app'>
+<NavaBar/>
+      
+    </div>
+  )
+}
+
+export default App
